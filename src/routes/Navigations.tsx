03b9a955577d2e8477c62a7db718aa2aa186f22e
jhonatanleon-carroya <jhonatan.leon@carroya.com>
2022-02-24 00:02:26 -0500

    lazy load y rutas no lazy load mas rutas hijas

--- src/routes/Navigations.tsx
+++ src/routes/Navigations.tsx
@@ -37,7 +37,7 @@ export const Navigations = () => {
                 <Route key={route.to} path={route.path} element={<route.Component />}/>
               ))
             }
-            <Route path="*" element={<Navigate  to={routes[0].to} replace />}/>
+            <Route path="/*" element={<Navigate  to={routes[0].to} replace />}/>
           </Routes>
           
         </div>
